20d4ee9ec1744389cbe1c730c3a580b0b12d1465
Chwen <zzero.i.n.g@gmail.com>
2024-03-06 16:19:50 +0630

    assignment commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Products</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Product List</h1>
+        <table class="table table-success table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">created_at</th>
+                    <th scope="col">updated_at</th>
+                </tr>
+            </thead>
+            <tbody>
+                {{-- @dd($products); --}}
+                @foreach ($products as $product)
+                    <tr>
+                        <th scope="row">{{ $product->id }}</th>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->description }}</td>
+                        <td>{{ $product->status }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->created_at }}</td>
+                        <td>{{ $product->updated_at }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
